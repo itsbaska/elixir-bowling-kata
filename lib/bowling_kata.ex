cf20ec7aea51be03eac8f315f984bbbe133a0945
Baska Batbold <baskabatbold@gmail.com>
2018-05-17 09:30:47 -0500

    add comments to functions

--- lib/bowling_kata.ex
+++ lib/bowling_kata.ex
@@ -3,24 +3,27 @@ defmodule BowlingKata do
   Documentation for BowlingKata.
   """
 
-  # reduce all the scores if it matches the [[frame] | []].
+  # reduce scores in a single frame if it matches [[frame] | []]
+  # [[frame] | []] ==> results to something like [[1, 2]]
+
   def score([frame | []]) do
     IO.puts "1-"
+    IO.inspect [frame | []]
     IO.inspect Enum.reduce(frame, 0, fn(x, total_score) -> x + total_score end)
   end
 
   def score([[10 |_] | rest]) do
     IO.puts "2-"
-    
-    10 + next_two_roll_score(rest) + score(rest)  
+    IO.inspect 10 + next_two_roll_score(rest) + score(rest)  
   end
 
+  # calculates score for when the frame adds up to 10 and is a spare.
   def score([[roll1, roll2] | rest]) when roll1 + roll2 == 10 do
     IO.puts "3-"
-    
     10 + next_roll(rest) + score(rest)
   end
 
+# 
   def score([frame | rest]) do
     IO.puts "4-"
     IO.puts "  "
@@ -28,27 +31,30 @@ defmodule BowlingKata do
     # rest ==> [[1, 2]]
     # score(rest) ==> 3
     IO.puts "  "
-    score([frame]) + score(rest)
+    IO.inspect score([frame]) + score(rest)
   end
 
+  # gives back the next roll
   def next_roll([[roll | _ ] | _]) do
     IO.puts "5-"
-    roll
+    IO.inspect roll
   end
 
+  # gives back the addition of the next two rolls, if they are not a strike
   def next_two_roll_score([[roll1, roll2, _]]) do
     IO.puts "6-"
     roll1 + roll2 
   end
 
+  # when there is a strike 
   def next_two_roll_score([[10 | _] | rest]) do
     IO.puts "7-"
     10 + next_roll(rest)
   end
 
+
   def next_two_roll_score([frame | _ ]) do
     IO.puts "8-"
-    score([frame])
+    IO.inspect score([frame])
   end
-
 end
--- test/bowling_kata_test.exs
+++ test/bowling_kata_test.exs
@@ -19,7 +19,7 @@ defmodule BowlingKataTest do
   end
 
   test "spare" do
-    assert BowlingKata.score([[1, 9], [0, 0]]) == 10
+    assert BowlingKata.score([[1, 9], [1, 0]]) == 12
   end
 
   test "strike" do
@@ -38,56 +38,26 @@ defmodule BowlingKataTest do
     assert BowlingKata.score([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]]) == 300
   end
 
-  test "game" do
-    assert BowlingKata.score([[0, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]]) == 270
+
+  # -----
+
+  test " all gutter ball score sheet" do 
+    knocked_pins = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    assert BowlingKata.score(knocked_pins) == 0
   end
 
-  # test "calculate score for spare frame" do 
-  #   assert BowlingKata.frame_score([0, "/"], [[1, 1], [nil, nil]]) == 11
-  # end 
+  test "all open frame score total" do
+    knocked_pins = [[2, 3], [4, 5], [6, 1], [8, 1], [1, 0], [2, 3], [4, 5], [6, 1], [8, 1], [1, 0, 0]]
+    assert BowlingKata.score(knocked_pins) == 62
+  end
 
-  # test "calculate score for two strike frames in a row" do 
-  #   assert BowlingKata.frame_score(["X", nil], [["X", nil], [0, 0]]) == 20
-  # end 
+  test "spare score total" do
+    knocked_pins = [[3, 5], [5, 5], [4, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0, 0]]
+    assert BowlingKata.score(knocked_pins) == 26
+  end
 
-  # test "calculate score for three strike frames in a row" do 
-  #   assert BowlingKata.frame_score(["X", nil], [["X", nil], ["X", nil]]) == 30
-  # end 
-
-  # test "calculate score for strike frame" do 
-  #   assert BowlingKata.frame_score(["X", 0], [[1, 1], [nil, nil]]) == 12
-  # end 
-
-
-
-  # test " all gutter ball score sheet" do 
-  #   knocked_pins = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-  #   assert BowlingKata.score(knocked_pins) == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  # end
-
-
-  # test "all open frame score sheet" do
-  #   knocked_pins = [[2, 3], [4, 5], [6, 7], [8, 9], [1, 0], [2, 3], [4, 5], [6, 7], [8, 9], [1, 0]]
-  #   assert BowlingKata.score(knocked_pins) == [5, 9, 13, 17, 1, 5, 9, 13, 17, 1]
-  # end
-
-  # test "all gutter ball score total" do 
-  #   knocked_pins = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-  #   assert BowlingKata.total_score(knocked_pins) == 0
-  # end
-
-  # test "all open frame score total" do
-  #   knocked_pins = [[2, 3], [4, 5], [6, 7], [8, 9], [1, 0], [2, 3], [4, 5], [6, 7], [8, 9], [1, 0]]
-  #   assert BowlingKata.total_score(knocked_pins) == 90
-  # end
-
-  # test "spare score total" do
-  #   knocked_pins = [[3, 5], [0, "/"], [4, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-  #   assert BowlingKata.total_score(knocked_pins) == 26
-  # end
-
-  # test "strike score total" do
-  #   knocked_pins = [[3, 5], ["X", 0], [4, 2], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-  #   assert BowlingKata.total_score(knocked_pins) == 30
-  # end
+  test "strike score total" do
+    knocked_pins = [[3, 5], [10, 0], [4, 2], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0, 0]]
+    assert BowlingKata.score(knocked_pins) == 30
+  end
 end
