58a9bc77f307f5a63e0e7f2dd648290c5136ec54
Baska Batbold <baskabatbold@gmail.com>
2018-05-17 13:03:25 -0500

    clear existing code

--- lib/bowling_kata.ex
+++ lib/bowling_kata.ex
@@ -3,41 +3,6 @@ defmodule BowlingKata do
   Documentation for BowlingKata.
   """
 
-  # def score([[roll1, roll2]]) do
-  #   roll1 + roll2
-  # end
-
-  def score([frame | []]) do
-    Enum.reduce(frame, 0, fn(roll, rolls) -> roll + rolls end)
-  end
-
-  def score([[10, _] | next_frames]) do
-    10 + next_two_rolls(next_frames) + score(next_frames)
-  end
-
-  def score([[roll1, roll2] | next_frames]) when roll1 + roll2 == 10 do
-    10 + next_roll(next_frames) + score(next_frames)
-  end
-
-  def score([frame | next_frames]) do
-    score([frame]) + score(next_frames)
-  end
-
-  def next_roll([[roll | _] | _]) do
-    roll
-  end
-  
-  def next_two_rolls([[roll1, roll2, _]]) do
-    roll1 + roll2
-  end
-
-  def next_two_rolls([[10, _] | next_frames]) do
-    10 + next_roll(next_frames)
-  end
-
-  def next_two_rolls([frame | _]) do 
-    score([frame])  
-  end
 
 
 
--- test/bowling_kata_test.exs
+++ test/bowling_kata_test.exs
@@ -2,38 +2,6 @@ defmodule BowlingKataTest do
   use ExUnit.Case
   doctest BowlingKata
 
-  test "gutter ball score" do
-    assert BowlingKata.score([[0, 0]]) == 0
-  end
-
-  test "open frame score" do
-    assert BowlingKata.score([[1, 0]]) == 1
-  end
-
-  test "last frame score" do
-    assert BowlingKata.score([[1, 2, 3]]) == 6
-  end
-
-  test "spare score" do
-    assert BowlingKata.score([[4, 6], [1, 2]]) == 14
-  end
-
-  test "strike score" do
-    assert BowlingKata.score([[10, 0], [1, 2]]) == 16
-  end
-
-  test "2 strike" do
-    assert BowlingKata.score([[10, 0], [10, 0], [1, 2]]) == 37
-  end
-
-  test "spare and strike" do
-    assert BowlingKata.score([[3,7], [10, 0], [1, 2]]) == 36
-  end
-
-  test "perfect game" do
-    assert BowlingKata.score([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]]) == 300
-    
-  end
 
 
 end
